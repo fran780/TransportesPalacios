685b30aa4fb4379c245d3265420d9b5854c828dd
mplemus <miltonjosuelemus@gmail.com>
2024-04-07 10:34:13 -0600

    Correccion Avance 1
    Cambios en el js y css

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    caruselInit();
-});
-
-function caruselInit(){
-
-    const nav = document.querySelector("header .nav");
+document.addEventListener("DOMContentLoaded", () => {
     const abrir = document.querySelector("header .abrir-menu");
     const cerrar = document.querySelector("header .cerrar-menu");
-    
-    abrir.addEventListener("click",()=>{
-        nav.classList.add("visible");
+
+    abrir.addEventListener("click", () => {
+        toggleMenu();
     });
-    
-    cerrar.addEventListener("click",()=>{
-        nav.classList.remove("visible");
+
+    cerrar.addEventListener("click", () => {
+        toggleMenu();
     });
-}
+
+    function toggleMenu() {
+        const nav = document.querySelector("header .nav");
+        nav.classList.toggle("visible");
+    }
+});
