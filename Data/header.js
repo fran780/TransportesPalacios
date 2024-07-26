3353050699a47be69771dfe98127a48e24dc2992
mp_lemus <miltonjosuelemus@gmail.com>
2024-07-26 03:21:04 -0600

    Actualizacion
    Modificacion de Pagina de Inicio, Acercade,Programas, pasadas a less.
    
    Menu hamburguesa, implementado con hbs y uso de variables.

---
+++ Data/header.js
@@ -0,0 +1,46 @@
+const header = {
+    header: [
+        {
+            item: "Inicio",
+            url: "/index.html"
+        },
+        {
+            item: "About",
+            url: "/pages/acercade.html"
+        },
+        {
+            item: "Programs",
+            url: "/pages/programas.html"
+        },
+        {
+            item: "Experiences",
+            url: "/pages/Experiencia.html"
+        },
+        {
+            item: "Gallery",
+            url: "/pages/galeria.html"
+        },
+        {
+            item: "Blog",
+            url: "/pages/blog.html"
+        },
+        {
+            item: "Events",
+            url: "/pages/eventos.html"
+        },
+        {
+            item: "Contact",
+            url: "/pages/contacto.html"
+        },
+        {
+            item: "Donations",
+            url: "/pages/donaciones.html"
+        },
+        {
+            item: "FAQ",
+            url: "/pages/preguntas.html"
+        }
+    ]
+};
+
+export default header;
--- Data/index.js
+++ Data/index.js
@@ -1,12 +1,14 @@
 import fotos from "./galeria";
 import preguntas from "./preguntas";
+import header from "./header";
 
 
 const getPageContext = (page) =>{
 
     return {
         ...preguntas,
-        ...fotos
+        ...fotos,
+        ...header
     };
 
 }
