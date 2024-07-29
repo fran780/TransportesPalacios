13efa6eea1eeb9f3e008b832ef13faa678c0b055
fran780 <franciscofernandez365@yahoo.com>
2024-07-29 14:11:58 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './Data';
 
 export default defineConfig({
-    base: "/transportespalacios/",
+    base: "",
     appType: 'mpa',
     build: {
         rollupOptions: {
